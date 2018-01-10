c3e6b607cdbd1c52f272a49609184de160cfc2bc
Darian Lio <14dl50@queensu.ca>
2018-01-10 14:58:38 -0500

    added delete todo func

--- ToDo/assets/js/index.js
+++ ToDo/assets/js/index.js
@@ -1,3 +1,23 @@
 $("li").click(function(){
-	if($(this).toggleCLass(completed"){}
+	$(this).css(".completed");
+});
+
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("ul").click(function(){
+
+});
+
+$("#container").click(function(){
+
+});
+
+$("body").click(function(){
+
 });
