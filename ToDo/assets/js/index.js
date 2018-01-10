a5fc71367b03995b0eb01a746d2077834849ca72
Darian Lio <14dl50@queensu.ca>
2018-01-10 15:43:13 -0500

    added many features
    - icons
    - fade toggles
    - fixed margins and paddings
    - added font styles
    - added transitions
    - added more colors

--- ToDo/assets/js/index.js
+++ ToDo/assets/js/index.js
@@ -1,23 +1,26 @@
-$("li").click(function(){
-	$(this).css(".completed");
+//check off specific todo by clicking when li is clicked in ul
+$("ul").on("click", "li", function(){
+	$(this).toggleClass(".completed");
 });
 
-
-$("span").click(function(event){
+//click on x to delete todo by click X in span in ul
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
 
-$("ul").click(function(){
-
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grab new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
 });
 
-$("#container").click(function(){
-
-});
-
-$("body").click(function(){
-
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
