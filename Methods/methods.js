d1a947fa93512078e3f283cc6893d40dc7a6d5da
Darian Lio <14dl50@queensu.ca>
2018-01-10 02:18:31 -0500

    learning methods in jQuery

---
+++ Methods/methods.js
@@ -0,0 +1,18 @@
+//.text()
+$("h1").text();
+$("ul").text();
+$("h1").text("New Text!");
+
+//.html()
+
+//.attr()
+
+//.val()
+
+//.addClass()
+
+//.removeClass()
+
+//.toggleClass()
+
+//check for more methods on jquery website
